b79b0153fc12618c468737129cf496447c5a0783
versii-debug <jseandelrosario112@gmail.com>
2026-06-10 21:54:35 +0800

    Add files via upload

---
+++ components/hero.php
@@ -0,0 +1,31 @@
+<section class="hero">
+    <div class="hero-left">
+        <p class="small_txt">Hello, I'm</p>
+        <h1>Jsean Eduard C. Del Rosario</h1>
+        <h2>IT Student & Tech Enthusiast</h2>
+
+        <p class="hero_desc">
+            Work in progrss-put what I do here
+        </p>
+        <div class="hero-buttons">
+            <a href="#projects" class="button-prima">View Projects</a>
+            <a href="assets/resume.pdf" class="button-secon">Download Resume</a>
+        </div>
+    </div><!-- Short intro -->
+
+    <div class="hero-right">
+        <div class="glass-card">
+            <h3>Current Stack</h3>
+            <ul>
+                <li>HTML</li>
+                <li>CSS</li>
+                <li>PHP</li>
+                <li>C++</li>
+                <li>Java</li>
+                <li>Python</li>
+                <li>SQL</li>
+                <li>Networking</li>
+            </ul>
+        </div>
+    </div><!-- Skills -->
+</section>
---
+++ components/navbar.php
@@ -0,0 +1,10 @@
+<nav class="navbar"><!-- tells browsers & search engines that the content inside is for site navigation;  class=css -->
+    <div class="logo">VER.</div>
+
+    <ul class="nav-links"><!-- Unordered list of navigation options-->
+        <li><a href="#about">About</a></li>
+        <li><a href="#skills">Skills</a></li>
+        <li><a href="#projects">Projects</a></li>
+        <li><a href="#contact">Contact</a></li><!-- Links -->
+    </ul>
+</nav>
